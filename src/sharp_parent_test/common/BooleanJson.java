f5d8866f3344153d9e580cdf49093308a525f6a0
likexin <247659668@qq.com>
2018-12-26 16:16:21 +0800

    增加了随机数生成，String.valueOf()使用，修改了正则表达式判断字符 2018/12/26

--- src/sharp_parent_test/common/BooleanJson.java
+++ src/sharp_parent_test/common/BooleanJson.java
@@ -9,21 +9,26 @@ public class BooleanJson {
     public static void main(String[] args) {
 
         ReturnVo returnvo = new ReturnVo();
-        String sessionId = "1234567";
-        String json = null;
+        String sessionId = "1234567[。";
+        String json1 = null;
+        String json2 = null;
 
         returnvo.setErrcode("0");
         returnvo.setErrmsg("ok");
         returnvo.setErrdata(sessionId);
-        String json1 = getJsonFromBean(returnvo);
-        System.out.println(json1);
-        boolean security = json1.matches("[a-zA-Z0-9,:\"{}]+");
-        if (security) {
-            json = json1;
-        } else {
-            json = "数据可能被篡改";
-        }
+        System.out.println(String.valueOf(returnvo));
+        
+        String json = getJsonFromBean(returnvo);
         System.out.println(json);
+        boolean security = json.matches("[a-zA-Z0-9,:\"{}]+");
+        if (security) {
+            json1 = json;
+        } else {
+            json1 = "数据可能被篡改";
+        }
+        json2=json.replaceAll("[^a-zA-Z0-9,:\"{}]+","");
+        System.out.println(json1);
+        System.out.println(json2);
     }
 
     public static String getJsonFromBean(Object javaBean) {
